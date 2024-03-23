d92bf7b9e99651d3f092b6f4c643c6e8998ba666
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-03-23 19:01:52 +0600

    Project adding functionallity added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1bw0ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,6 +37,23 @@ async function run() {
     // Project related API
     app.get('/projects',async(req,res)=>{
         const result = await projectsCollections.find().toArray();
+        res.send(result)
+    });
+    app.get('/projects/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await projectsCollections.findOne(query);
+        res.send(result);
+    });
+    app.post('/projects',async(req,res)=>{
+        const item = req.body;
+        const result = await projectsCollections.insertOne(item);
+        res.send(result);
+    });
+    app.delete('/projects/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await projectsCollections.deleteOne(query);
         res.send(result)
     });
 
